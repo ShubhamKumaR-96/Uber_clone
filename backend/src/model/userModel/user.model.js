e7333364aaeda9b357f4d7ab694338e6601b80b6
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-02-15 15:20:51 +0530

    feat: updated userModel code

--- backend/src/model/userModel/user.model.js
+++ backend/src/model/userModel/user.model.js
@@ -1,48 +1,118 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
-  fullname: {
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+      select: false, // exclude password from query results by default
+    },
     firstName: {
       type: String,
-      required: true,
-      minlength: [3, "Firstname must be contains 3 characters as long "],
+      required: [true, "Please provide a first name"],
+      trim: true,
+      maxlength: [50, "First name cannot be more than 50 characters"],
     },
     lastName: {
       type: String,
-      minlength: [3, "lasttname must be contains 3 characters as long "],
+      required: [true, "Please provide a last name"],
+      trim: true,
+      maxlength: [50, "Last name cannot be more than 50 characters"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please provide a phone number"],
+      unique: true,
+      trim: true,
+      match: [/^\d{10}$/, "Please provide a valid 10-digit phone number"],
+    },
+    role: {
+      type: String,
+      enum: ["rider", "admin", "driver"],
+      default: "rider",
+    },
+    profileImage: {
+      type: String,
+      default: "https://via.placeholder.com/150",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 5,
+    },
+    totalRides: {
+      type: Number,
+      default: 0,
     },
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true,
-    minlength:[5, "email must be contains atleast 5 chars"]
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  password:{
-    type:String,
-    required:true
-  },
-  socketId:{
-    type:String
-  }
+);
 
+userSchema.virtual("fullname").get(function () {
+  return `${this.firstName} ${this.lastName}`;
 });
 
-userSchema.methods.generateAuthToken=function(){
-  const token=jwt.sign({_id:this._id},process.env.JWT_SECRET)
-  return token
-}
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
-userSchema.methods.comparePassword= async function (password){
-  return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
-userSchema.statics.hashPassword= async function (password){
-  return await bcrypt.hash(password,10)
-}
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    { id: this._id, email: this.email, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN || "1h" },
+  );
+};
 
-const UserModel=mongoose.model("user",userSchema)
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { id: this._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d" },
+  );
+};
 
-export default UserModel
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
